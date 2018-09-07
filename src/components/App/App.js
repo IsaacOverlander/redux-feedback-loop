d867671a7c21d8959c6db37c74095825daaf3274
Unknown <Isaac.overlander@gmail.com>
2018-09-07 16:26:57 -0500

    Created client side routing in App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import './App.css';
+import AdminView from '../AdminView/AdminView.js';
+import CommentsView from '../CommentsView/CommentsView.js';
+import FeelingView from '../FeelingView/FeelingView.js';
+import SuccessView from '../SuccessView/SuccessView.js';
+import SupportView from '../SupportView/SupportView.js';
+import UnderstandingView from '../UnderstandingView/UnderstandingView.js';
+import { HashRouter as Router, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
-        </header>
-        <br/>
-        
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Feedback!</h1>
+            <h4><i>Don't forget it!</i></h4>
+          </header>
+          <br />
+          <Route path="/" component={FeelingView} />
+          <Route path="/understanding" component={UnderstandingView} />
+          <Route path="/support" component={SupportView} />
+          <Route path="/comments" component={CommentsView} />
+          <Route path="/success" component={SuccessView} />
+          <Route path="/admin" component={AdminView} />
+        </div>
+      </Router>
     );
   }
 }
